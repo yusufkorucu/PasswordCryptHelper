f8c9ed21eae87b39730e36b1e6f845c4859a4877
KORUCU <yusuf.korucu@bilgeadam.com>
2023-05-28 00:00:55 +0300

    Add project files.

---
+++ PasswordCryptHelper/Program.cs
@@ -0,0 +1,13 @@
+﻿using PasswordCryptHelper.Helpers;
+
+Console.Write("Enter a password: ");
+
+string passwordInput = Console.ReadLine();
+
+string response = Helper.Hash(passwordInput);
+
+string password = "123";
+
+var verificationResult = Helper.Validate(password:password,passwordHash:response);
+
+Console.WriteLine(verificationResult.ToString());
